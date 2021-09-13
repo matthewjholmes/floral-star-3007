63b85a7f21983096a7a770f21234e651454d5f8b
matthewjholmes <ilookup@gmail.com>
2021-09-13 11:51:42 -0600

    Minor refactor

--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -3,6 +3,6 @@ class Mechanic < ApplicationRecord
   has_many :rides, through: :mechanic_rides
 
   def self.average_years_experience
-    Mechanic.average(:years_experience)
+    average(:years_experience)
   end
 end
